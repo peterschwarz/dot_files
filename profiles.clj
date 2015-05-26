4324fbd796edb1af3378aa46344efb88b1df35c0
Peter Schwarz <peter.schwarz@bicycle.io>
2015-05-26 15:03:40 -0500

    added deps for vim-fireplace plugins

--- profiles.clj
+++ profiles.clj
@@ -2,9 +2,13 @@
   {:plugins [[lein-try "0.4.3"]
              [lein-exec "0.3.4"]
              [lein-ancient "0.6.1"]
+             [lein-cljfmt "0.1.10"]
              [jonase/eastwood "0.2.1"]
              [lein-plz "0.3.1" :exclusions [[rewrite-clj] [ancient-clj]]]
              [slamhound "1.5.5"]
+             [cider/cider-nrepl "0.8.2"]
              [venantius/ultra "0.3.2"]]
+   :dependencies [[jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
+                  [cljfmt "0.1.10"]]
    :ultra {:color-scheme :solarized_dark}
    :repl-options {:port 0}}}
