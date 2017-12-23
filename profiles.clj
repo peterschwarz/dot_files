1c93437c573c88133120ed8a6365cb17ce1c967b
Peter Schwarz <peter.schwarz@bicycle.io>
2017-12-23 14:01:51 -0600

    Updated lein profile
    Signed-off-by: Peter Schwarz <peter.schwarz@bicycle.io>

--- profiles.clj
+++ profiles.clj
@@ -1,17 +1,10 @@
 {:user
   {:plugins [[lein-try "0.4.3"]
-             [lein-exec "0.3.6"]
-             [lein-ancient "0.6.10"]
-             [lein-cljfmt "0.5.0"]
-             [jonase/eastwood "0.2.3"]
-             [cider/cider-nrepl "0.11.0"]
-             [venantius/ultra "0.4.1"]]
-   :dependencies [[cljfmt "0.3.0"]
-                  [com.cemerick/pomegranate "0.3.1"]]
+             [lein-ancient "0.6.15"]
+             [lein-cljfmt "0.5.7"]
+             [cider/cider-nrepl "0.15.1"]
+             [venantius/ultra "0.5.2"]]
+   :dependencies [[cljfmt "0.5.7"]]
    :ultra {:color-scheme :solarized_dark}
    :repl-options {:port 0
-                  :init (do
-                          (require 'cljfmt.core)
-                          (defn current-classpath []
-                            (->> (ClassLoader/getSystemClassLoader) (.getURLs) seq (map #(.getFile %))))
-                          )}}}
+                  :init (require 'cljfmt.core)}}}
